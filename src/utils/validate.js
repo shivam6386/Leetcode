7d8976d0baeccf601633264ca68b4988ec31f6c5
shivam <shivammanitripathishiv@gmail.com>
2025-12-28 16:01:40 +0530

    New User Validation

---
+++ src/utils/validate.js
@@ -0,0 +1,17 @@
+const valiadtor=require('validator')
+const registerValidation=function(data){
+
+    const manditoryField=["Fristname","email" , "password" ];
+    const requiredField=manditoryField.every((K)=>Object.keys(data).includes(k));
+     
+    if(!requiredField){
+        throw new Error("Fill the missing field");
+        
+    }
+    if (!validator.isEmail(data.email)){
+   throw new Error ('InValid Email');
+    }
+   
+}
+
+module.exports=registerValidation;
