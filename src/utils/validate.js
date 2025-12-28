f3cb271245467945e02468064c9b2381b5636893
shivam <shivammanitripathishiv@gmail.com>
2025-12-28 16:11:19 +0530

    Correct typo mistake

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,19 +1,21 @@
-const valiadtor=require('validator')
-const registerValidation=function(data){
+const validator = require('validator');
 
-    const manditoryField=["Fristname","email" , "password" ];
-    const requiredField=manditoryField.every((K)=>Object.keys(data).includes(k));
-     
-    if(!requiredField){
+const registerValidation = function(data) {
+    const mandatoryFields = ["firstname", "email", "password"];
+    
+    const allFieldsPresent = mandatoryFields.every((key) => Object.keys(data).includes(key));
+    
+    if (!allFieldsPresent) {
         throw new Error("Fill the missing field");
-        
     }
-    if (!validator.isEmail(data.email)){
-   throw new Error ('InValid Email');
+    
+    if (!validator.isEmail(data.email)) {
+        throw new Error('Invalid Email');
     }
-   if (!validator.isStrongPassword(data.password)){
-   throw new Error ('InValid Email');
+    
+    if (!validator.isStrongPassword(data.password)) {
+        throw new Error('Invalid Password');
     }
 }
 
-module.exports=registerValidation;
+module.exports = registerValidation;
